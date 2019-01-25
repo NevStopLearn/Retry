59a9277e7b05707548b31bc6b11ee3ff821083d5
BuLing <BuLing@qq.com>
2019-01-25 15:39:06 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,48 @@ class User extends Authenticatable
     {
         return $this->statuses()->latest();
     }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class,'followers','user_id','follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::class,'followers','follower_id','user_id');
+    }
+
+    /**
+     * 关注用户
+     * @param $user_ids
+     */
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids,false);
+    }
+
+    /**
+     * 取消关注
+     * @param $user_ids
+     */
+    public function unfollow($user_ids)
+    {
+         if (!is_array($user_ids)) {
+             $user_ids = compact('user_ids');
+         }
+         $this->followings()->detach($user_ids);
+    }
+
+    /**
+     * 判断当前用户是否关注XX
+     * @param $user_id
+     * @return mixed
+     */
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
